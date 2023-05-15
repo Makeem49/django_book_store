7f81ca979d1e6448df7fd31798d53cc9dcfc2eec
Moshood <moshoodakeemolayinka@gmail.com>
2023-05-15 22:37:54 +0100

    add signup page

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,4 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("pages.urls"))]
+urlpatterns = [
+    # Django Admin
+    path("admin/", admin.site.urls),
+    # User Management
+    path("accounts/", include("django.contrib.auth.urls")),
+    # Local Apps
+    path("accounts/", include("accounts.urls")),
+    path("", include("pages.urls")),
+]
